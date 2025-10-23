7e2f2d7fd24a3d00ef7843aadd9337d6bc650825
KartanoDev <albertomartinezmontoya@gmail.com>
2025-10-23 15:09:45 +0200

    cambio entorno pro.

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // Esta URL se reemplazará durante el despliegue en Vercel
-  apiBaseUrl: process.env['NG_APP_API_BASE_URL'] || 'https://learning-cards-a35l.onrender.com/api'
+  // Apuntamos a la URL del backend desplegado en Render
+  apiBaseUrl: 'https://learning-cards-a35l.onrender.com/api'
 };
