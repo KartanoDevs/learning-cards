3747ceb9b5431792f4321e3adff4ab892002835d
KartanoDev <albertomartinezmontoya@gmail.com>
2025-10-23 13:39:51 +0200

    Commit

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://learning-cards-a35l.onrender.com/api'
+  // Esta URL se reemplazará durante el despliegue en Vercel
+  apiBaseUrl: process.env['NG_APP_API_BASE_URL'] || 'https://learning-cards-a35l.onrender.com/api'
 };
